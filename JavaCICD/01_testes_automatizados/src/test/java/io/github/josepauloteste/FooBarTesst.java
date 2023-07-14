492a5eb8aea6c123ca4386420f6dfdc7da3e73bf
jose.brandao <jose.brandao@americanas.io>
2023-07-14 16:36:01 -0300

    first commit

---
+++ JavaCICD/01_testes_automatizados/src/test/java/io/github/josepauloteste/FooBarTesst.java
@@ -0,0 +1,13 @@
+package io.github.josepauloteste;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTesst {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("Hello junit");
+	}
+
+}
