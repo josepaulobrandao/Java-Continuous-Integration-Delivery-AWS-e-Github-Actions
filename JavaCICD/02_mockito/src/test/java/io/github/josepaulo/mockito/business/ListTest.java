322ab6c54949332c7f2eac55ce3d05866dac6aaa
jose.brandao <jose.brandao@americanas.io>
2023-07-17 16:06:57 -0300

    Adicionando testes unitarios [desafio]

--- JavaCICD/02_mockito/src/test/java/io/github/josepaulo/mockito/business/ListTest.java
+++ JavaCICD/02_mockito/src/test/java/io/github/josepaulo/mockito/business/ListTest.java
@@ -1,71 +1,72 @@
 package io.github.josepaulo.mockito.business;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class ListTest {
 
-	// test[System Under Test]_[Condition or State Change]_[Expected Result]
 	@Test
-	@DisplayName("Display Name")
-	void testMockingList_when_SizeIsCalled_shouldReturn10() {
-		// Given - Arrange
-		List<?> list = mock(List.class);
-		when(list.size()).thenReturn(10);
-
-		// Then - Assert && // When - Act
-		assertEquals(10, list.size());
-		assertEquals(10, list.size());
-		assertEquals(10, list.size());
-		assertEquals(10, list.size());
+	public void mockingListSizeTest() {
+		// Given / Arrange
+		List list = mock(List.class);
+		given(list.size()).willReturn(10);
+		// When / Act && Then / Assert
+		assertThat(list.size(), is(10));
+		assertThat(list.size(), is(10));
+		assertThat(list.size(), is(10));
 	}
 
 	@Test
-	@DisplayName("Display Name")
-	void testMockingList_when_SizeIsCalled_shouldReturnMultipleValues() {
-		// Given - Arrange
-		List<?> list = mock(List.class);
-		when(list.size()).thenReturn(10).thenReturn(20);
-
-		// Then - Assert && // When - Act
-		assertEquals(10, list.size());
-		assertEquals(20, list.size());
-		assertEquals(20, list.size());
+	public void mockingListSize_withMultipleReturnValues() {
+		// Given / Arrange
+		List list = mock(List.class);
+		given(list.size()).willReturn(10).willReturn(20);
+		// When / Act && Then / Assert
+		assertThat(list.size(), is(10)); // First Call
+		assertThat(list.size(), is(20)); // Second Call
 	}
 
 	@Test
-	@DisplayName("Display Name")
-	void testMockingList_when_GetIsCalledWhitArgumentMacher_ShouldReturnErudio() {
-		// Given - Arrange
-		List<String> list = mock(List.class);
-		when(list.get(anyInt())).thenReturn("Erudio");
-
-		// Then - Assert && // When - Act
-		assertEquals("Erudio", list.get(anyInt()));
-		assertEquals("Erudio", list.get(anyInt()));
-
+	public void mockingListGet() {
+		// Given / Arrange
+		var list = mock(List.class);
+		given(list.get(0)).willReturn("Erudio");
+		// When / Act && Then / Assert
+		assertThat(list.get(0), is("Erudio"));
+		assertNull(list.get(1));
 	}
 
 	@Test
-	@DisplayName("Display Name")
-	void testMockingList_when_ThrowsAnException() {
-		// Given - Arrange
-		List<String> list = mock(List.class);
-		when(list.get(anyInt())).thenThrow(new RuntimeException("Foo bar!!"));
+	public void mockingListGet_withAny() {
 
-		// Then - Assert && // When - Act
-		assertThrows(RuntimeException.class, () -> { list.get(anyInt());
-		}, () -> "Should have thorw an RuntimeException");
-		assertEquals("Erudio", list.get(anyInt()));
+		// Given / Arrange
+		var list = mock(List.class);
+		given(list.get(Mockito.anyInt())).willReturn("Erudio");
 
+		// When / Act && Then / Assert
+		assertThat(list.get(0), is("Erudio"));
+		assertThat(list.get(1), is("Erudio"));
 	}
 
+	@Test
+	public void mockingList_throwAnException() {
+		// Given / Arrange
+		var list = mock(List.class);
+		given(list.get(Mockito.anyInt())).willThrow(new RuntimeException("Foo Bar!!"));
+
+		// When / Act && Then / Assert
+		assertThrows(RuntimeException.class, () -> {
+			// When / Act
+			list.get(0);
+		}, () -> "Should have throw an RuntimeException");
+	}
 }
